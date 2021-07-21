75e82078bd796f72741440a3831a86663d5d7768
GaborIreHun <76515173+GaborIreHun@users.noreply.github.com>
2021-07-21 17:56:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-  const fastify = require('./server')
-  const start = async () => {
-    try {
-      await fastify.listen(3000)
-    } catch (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
+const fastify = require('./server')({logger: true})
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
   }
-  start()
+}
+start()
